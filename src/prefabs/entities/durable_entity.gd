b7f37dcf4e294523489fdfdff9d354420089afbc
kesha <73757143+JustKesha@users.noreply.github.com>
2026-07-11 23:16:18 +0400

    feat: Add durable entity prefab

---
+++ src/prefabs/entities/durable_entity.gd
@@ -0,0 +1,27 @@
+@icon("uid://dnj0aatchqrxp")
+class_name DurableEntity3D
+extends Entity3D
+
+
+@export var account_for_damage_taken: bool = true
+@export var account_for_damage_dealt: bool = true
+@export var self_damage_on_damage_taken: float
+@export var self_damage_on_damage_dealt: float
+
+@onready var health: HealthComponent = %Health
+
+
+func _on_hurtbox_damage_taken(amount: float, source: HitComponent3D):
+	health.decrease(self_damage_on_damage_taken
+		+ (amount if account_for_damage_taken else 0.0))
+	super(amount, source)
+
+
+func _on_hitbox_hit(hurt_component: HurtComponent3D):
+	health.decrease(self_damage_on_damage_dealt
+		+ (hitbox.damage if account_for_damage_dealt else 0.0))
+	super(hurt_component)
+
+
+func _on_health_minimum_value_reached():
+	destroy()
