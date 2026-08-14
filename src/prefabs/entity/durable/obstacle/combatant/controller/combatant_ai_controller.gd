13c0d9761604a0e9d2366590ef3d525050c3a7a3
kesha <73757143+JustKesha@users.noreply.github.com>
2026-08-14 18:44:32 +0400

    feat: Add CombatantAIController3D
    This one I dont really like tbh, at least not the public api; Might be a subject to refactor later

---
+++ src/prefabs/entity/durable/obstacle/combatant/controller/combatant_ai_controller.gd
@@ -0,0 +1,107 @@
+@icon("uid://cwdhm7f8w4uko")
+class_name CombatantAIController3D
+extends CombatantController3D
+
+
+@export var target: Entity3D
+@export var target_alignment_margin: float = 0.5
+@export var update_time: float = 1.0:
+	set(value):
+		update_time = value
+		if _update_timer:
+			_update_timer.wait_time = update_time
+			if _update_timer.time_left > update_time:
+				_update_timer.start()
+
+var _update_timer: Timer
+
+
+func _ready():
+	super()
+	
+	_update_timer = Timer.new()
+	_update_timer.name = "UpdateTimer"
+	_update_timer.wait_time = update_time
+	_update_timer.autostart = true
+	_update_timer.one_shot = false
+	_update_timer.timeout.connect(_on_update_timer_timeout)
+	add_child(_update_timer)
+
+
+func _on_update_timer_timeout():
+	if not disabled:
+		update()
+
+
+func _pick_target(available_targets: Array[Entity3D]) -> Entity3D:
+	if not available_targets:
+		return null
+	return available_targets.pick_random()
+
+
+func is_valid_target(entity: Entity3D) -> bool:
+	if not combatant:
+		return false
+	return entity and entity.can_process() and entity.team != combatant.team
+
+
+func is_target_aligned() -> bool:
+	return get_alignment_offset(target) <= target_alignment_margin
+
+
+func get_movement_direction() -> Vector3:
+	if not combatant or not target or is_target_aligned():
+		return Vector3.ZERO
+	
+	var target_direction := target.global_position - combatant.global_position
+	
+	target_direction.z = 0.0
+	
+	return target_direction.normalized()
+
+
+func get_shooting_state() -> bool:
+	return is_target_aligned()
+
+
+func get_alignment_offset(from: Entity3D) -> float:
+	if not combatant or not from:
+		return INF
+	
+	var player_pos_2d := Vector2(combatant.global_position.x, combatant.global_position.y)
+	var target_pos_2d := Vector2(from.global_position.x, from.global_position.y)
+	
+	var distance_off_match := player_pos_2d.distance_to(target_pos_2d)
+	
+	return distance_off_match
+
+
+func get_available_targets() -> Array[Entity3D]:
+	if not combatant or not Game.current_level:
+		return []
+	var available_targets: Array[Entity3D]
+	available_targets.assign(Game.current_level.get_obstacles([combatant.team], true))
+	return available_targets
+
+
+func get_next_target() -> Entity3D:
+	return _pick_target(get_available_targets())
+
+
+func switch_target():
+	target = get_next_target()
+
+
+func update_target():
+	if not is_valid_target(target):
+		switch_target()
+
+
+func update():
+	super()
+	update_target()
+
+
+func reset():
+	super()
+	target = null
