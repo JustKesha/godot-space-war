03998f419267086f3f27ef783482a44d69c68d73
kesha <73757143+JustKesha@users.noreply.github.com>
2026-08-14 18:42:28 +0400

    feat: Add base abstract CombatantController3D

---
+++ src/prefabs/entity/durable/obstacle/combatant/controller/combatant_controller.gd
@@ -0,0 +1,46 @@
+@abstract
+@icon("uid://5jiu50ljkcni")
+class_name CombatantController3D
+extends Node3D
+
+
+@export var combatant: Combatant3D
+@export var disabled: bool
+
+
+func _ready():
+	if not combatant:
+		combatant = get_parent() as Combatant3D
+	if not combatant:
+		push_warning("Combatant is not selected and not found as parent.")
+	
+	reset.call_deferred()
+
+
+func get_movement_direction() -> Vector3:
+	return Vector3.ZERO
+
+
+func get_shooting_state() -> bool:
+	return false
+
+
+func update_movement_direction():
+	if combatant:
+		combatant.movement.direction = get_movement_direction()
+
+
+func update_shooting_state():
+	if combatant:
+		combatant.gun.auto_shoot = get_shooting_state()
+
+
+func update():
+	update_movement_direction()
+	update_shooting_state()
+
+
+func reset():
+	if combatant:
+		combatant.movement.direction = Vector3.ZERO
+		combatant.gun.auto_shoot = false
