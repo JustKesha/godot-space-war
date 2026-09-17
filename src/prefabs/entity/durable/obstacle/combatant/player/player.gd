621d8822e2b5e073552cf8b94e2f2967ec949e69
kesha <73757143+JustKesha@users.noreply.github.com>
2026-09-17 17:23:27 +0400

    feat: Add global events for player spawned/destroyed

--- src/prefabs/entity/durable/obstacle/combatant/player/player.gd
+++ src/prefabs/entity/durable/obstacle/combatant/player/player.gd
@@ -7,8 +7,19 @@ extends Combatant3D
 @onready var controller: CombatantPlayerController = %Controller
 
 
+func _ready():
+	super()
+	Events.player_spawned.emit(self)
+
+
 func _dispose():
 	if Game.current_level:
 		Game.current_level.players.dispose(self)
 	else:
 		super()
+
+
+func destroy():
+	super()
+	var killer_signature := Utils.get_instance_signature(_last_damage_source)
+	Events.player_destroyed.emit(self, killer_signature)
