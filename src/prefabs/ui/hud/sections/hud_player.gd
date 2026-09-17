3e9bdc7d94c618e804beb719abef85573ec7a663
kesha <73757143+JustKesha@users.noreply.github.com>
2026-09-17 18:17:18 +0400

    feat: Add score (xp) to player hud section

--- src/prefabs/ui/hud/sections/hud_player.gd
+++ src/prefabs/ui/hud/sections/hud_player.gd
@@ -13,6 +13,7 @@ extends HUDSection
 
 @onready var nickname: Label = %Nickname
 @onready var health: Label = %Health
+@onready var score: Label = %Score
 
 
 func _connect_signals():
@@ -20,6 +21,7 @@ func _connect_signals():
 		return
 	player.health.value_changed.connect(_on_player_health_changed)
 	player.health.maximum_value_changed.connect(update)
+	player.score.value_changed.connect(_on_player_score_changed)
 	player.destroyed.connect(_on_player_destroyed)
 	player.tree_exiting.connect(_on_player_exiting_tree)
 
@@ -31,10 +33,16 @@ func _disconnect_signals():
 		player.health.value_changed.disconnect(_on_player_health_changed)
 	if player.health.maximum_value_changed.is_connected(update):
 		player.health.maximum_value_changed.disconnect(update)
+	if player.score.value_changed.is_connected(_on_player_score_changed):
+		player.score.value_changed.disconnect(_on_player_score_changed)
 	if player.destroyed.is_connected(_on_player_destroyed):
 		player.destroyed.disconnect(_on_player_destroyed)
 	if player.tree_exiting.is_connected(_on_player_exiting_tree):
 		player.tree_exiting.disconnect(_on_player_exiting_tree)
+
+
+func _on_player_score_changed(_new_value: float):
+	update()
 
 
 func _on_player_health_changed(_new_value: float):
@@ -59,9 +67,8 @@ func _on_updated():
 	var hp_max := player.health.value_max
 	
 	nickname.text = player.name
+	score.text = "XP: " + Utils.format_number(player.score.value, 2)
 	health.text = (
 		"HP: " + Utils.format_number(hp, 2)
-		#"HP: " + (Utils.format_number(hp, 2) if not is_inf(hp) else "INF")
 		+ (("/" + Utils.format_number(hp_max, 2) if not is_inf(hp_max) else ""))
-	)
-	print(health.text)
+		)
